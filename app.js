c9b72738f05dd31e994b10e0194a6913613948e7
ariviere <antoine@riviere.tf>
2013-05-21 18:30:23 -0700

    parse xml

--- app.js
+++ app.js
@@ -4,16 +4,18 @@ http.createServer(function(request, response) {
 	response.writeHead(200);
 	response.write("Hello, this is dog.");
 	
-	SetInterval(getRssFeed, 3000);
 	
 	response.end();
 }).listen(8080);
 console.log('Listening on port 8080');
 
+setInterval(getRssFeed, 3000);
+
 var FeedParser = 	require('feedparser'), 
 					request = require('request');
 					
-var getRssFeed = new request('http://www.esportsfrance.com/rss/esports.rss')
+function getRssFeed(){
+	request('http://www.esportsfrance.com/rss/esports.rss')
 	  .pipe(new FeedParser())
 	  .on('error', function(error) {
   	    console.error(error);
@@ -24,3 +26,5 @@ var getRssFeed = new request('http://www.esportsfrance.com/rss/esports.rss')
 	  .on('end', function () {
 	   // do the next thing
 	  });
+};
+	  
