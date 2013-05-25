e61348de429929c56e7b188ed853b1ef7ba022b1
ariviere <antoine@riviere.tf>
2013-05-24 18:54:10 -0700

    Début intégration mongodb

--- app.js
+++ app.js
@@ -1,30 +1,92 @@
-var http = require('http');
+//Modules
+var http 			= require('http');
+var FeedParser 		= require('feedparser'), 
+request 			= require('request');
+var mongoose 		= require('mongoose');
 
+//Fichiers
+var articleClass 	= require('./article_class');
+
+//mongodb
+mongoose.connect('mongodb://localhost/test');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback(){
+	console.log("WORK");
+});
+
+var articleSchemaMongo = mongoose.Schema({
+	title: 			String,
+	website: 		String,
+	thumbnail: 		String,
+	link: 			String,
+	description: 	String,
+	pubDate: 		String,
+	author: 		String,
+	titleDate: 		String
+})
+
+var ArticleMongo = mongoose.model('Article', articleSchemaMongo);
+
+// ArticleMongo.find(function (err, articles){
+// 	console.log(articles);
+// })
+
+//Lancement du serveur http (pour les tests)
 http.createServer(function(request, response) {
 	response.writeHead(200);
 	response.write("Hello, this is dog.");
+	//var articleTest = new articleClass.Article("title", "website", "thumbnail", "url", "description", "pubDate", "author");
+	
+	getRssFeed();
 	
 	
 	response.end();
-}).listen(8080);
+}).listen(8081);
+
 console.log('Listening on port 8080');
 
-setInterval(getRssFeed, 3000);
 
-var FeedParser = 	require('feedparser'), 
-					request = require('request');
-					
+//Interval de temps entre chaque lancement du parser RSS
+//setInterval(getRssFeed, 3000);
+
+//Déclaration du tableau d'objets Article
+var rssArray = new Array();
+
+//Fonction de parsing xml			
 function getRssFeed(){
 	request('http://www.esportsfrance.com/rss/esports.rss')
 	  .pipe(new FeedParser())
 	  .on('error', function(error) {
   	    console.error(error);
 	  })
-	  .on('article', function (article) {
-  	    console.log('Got article: %s', article.title || article.description);
+	  .on('article', function(article) {
+		  rssArray.push(generateArticle(article));
 	  })
 	  .on('end', function () {
 	   // do the next thing
 	  });
+  
+	console.log(rssArray.length);
+  	for(i = 0 ; i < rssArray.length ; i++){
+  		console.log(rssArray[i].link);
+  	}
 };
-	  
+
+function generateArticle(article){
+	var articleSent = new articleClass.Article(article.title, "esfr", "sc2", article.link, article.description, article.pubDate, article.author);
+	var articleMongo = new ArticleMongo({ 
+		title: 			articleSent.title, 
+		website: 		articleSent.website,
+		thumbnail: 		articleSent.thumbnail,
+		link: 			articleSent.link,
+		description: 	articleSent.description,
+		pubDate: 		articleSent.pubDate,
+		author: 		articleSent.author,
+		titleDate: 		articleSent.title + "-" + articleSent.pubDate
+	});
+	articleMongo.save();
+	//console.log(article);
+	return articleSent;
+}
---
+++ article_class.js
@@ -0,0 +1,11 @@
+module.exports = {
+ Article: function(title, website, thumbnail, link, description, pubDate, author){
+	this.title = title;
+	this.website = website;
+	this.thumbnail = thumbnail;
+	this.link = link;
+	this.description = description;
+	this.pubDate = pubDate;
+	this.author = author;
+}
+};
