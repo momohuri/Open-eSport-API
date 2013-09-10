500a71b445b0ef38fb096483ab25e5da04d2377e
ariviere <antoine@riviere.tf>
2013-09-10 17:11:57 +0200

    api with sort and limit

--- api.js
+++ api.js
@@ -2,25 +2,27 @@ var mongoConfig		= require('./mongoconfig');
 var db 				= mongoConfig.db;
 	
 exports.findByLanguage = function(req, res) {
-	db.collection('articles').find({language: req.params.lang}).toArray(function(err, articles){
-		if(!err) res.send(articles);
+	db.collection('articles').find({language: req.params.lang}).sort({ pubDate: -1 }).limit(100).toArray(function(err, articles){
+		if(!err) {
+			res.send(articles);
+		}
 	});
 };
 
 exports.findByWebsite = function(req, res) {
-	db.collection('articles').find({website: req.params.website}).toArray(function(err, articles){
+	db.collection('articles').find({website: req.params.website}).sort({ pubDate: -1 }).limit(20).toArray(function(err, articles){
 		if(!err) res.send(articles);
 	});
 };
 
 exports.findByGame = function(req, res) {
-	db.collection('articles').find({categories: req.params.game}).toArray(function(err, articles){
+	db.collection('articles').find({categories: req.params.game}).sort({ pubDate: -1 }).limit(60).toArray(function(err, articles){
 		if(!err) res.send(articles);
 	});
 };
 
 exports.findByWebsiteAndGame = function(req, res) {
-	db.collection('articles').find({website: req.params.website, categories: req.params.game}).toArray(function(err, articles){
+	db.collection('articles').find({website: req.params.website, categories: req.params.game}).sort({ pubDate: -1 }).limit(20).toArray(function(err, articles){
 		if(!err) res.send(articles);
 	});
 };
