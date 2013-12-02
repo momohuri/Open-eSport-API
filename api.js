d5d4091fffb95801641312d59815a432e975f514
Antoine Riviere <aereivir@gmail.com>
2013-12-02 14:55:48 +0100

    game api with websites sorted results

--- api.js
+++ api.js
@@ -18,6 +18,38 @@ define([], function(){
 		}
 	};
 
+	function getArticlesFromWebsitesAndGame(websites, game, callback){
+		var allArticles = [];
+		var count = 0;
+		
+		for(var i = 0 ; i < websites.length ; i++){
+			if(game === 'others'){
+				db.collection('articles').find({ $and: [{ website: websites[i] }, { category: { $ne: 'lol' } }, { category: { $ne: 'sc2' } }, { category: { $ne: 'dota2' } }, { category: { $ne: 'csgo' } }] }).sort({ pubDate: -1 }).limit(40).toArray(function(err, articles){
+					if(!err) {
+						count++;
+						allArticles = allArticles.concat(articles);
+						if(count == websites.length){
+							count = 0;
+							callback(allArticles);
+						}
+					}
+				});
+			}
+			else{
+				db.collection('articles').find({ $and: [{ website: websites[i] }, { category: game } ]}).sort({ pubDate: -1 }).limit(40).toArray(function(err, articles){
+					if(!err) {
+						count++;
+						allArticles = allArticles.concat(articles);
+						if(count == websites.length){
+							count = 0;
+							callback(allArticles);
+						}
+					}
+				});
+			}
+		}
+	};
+
 	return{
 
 		findAll: function(req, res){
@@ -49,23 +81,17 @@ define([], function(){
 		},
 
 		findByGame: function(req, res) {
-			if(req.params.game === 'others'){
-				db.collection('articles').find({ $and: [ { category: { $ne: 'lol' } }, { category: { $ne: 'sc2' } }, { category: { $ne: 'dota2' } }, { category: { $ne: 'csgo' } },  ]}).sort({ pubDate: -1 }).limit(80).toArray(function(err, articles){
-					if(!err) res.send(articles);
-				});
-			}
-			else{
-				db.collection('articles').find({category: req.params.game}).sort({ pubDate: -1 }).limit(80).toArray(function(err, articles){
-					if(!err) res.send(articles);
-				});
-			}
+			var websites = ["O Gaming", "Team aAa", "Millenium", "VaKarM", "HLTV", "Reddit", "TeamLiquid", "IEWT", "Thunderbot", "onGamers", "SK Gaming"];
+			getArticlesFromWebsitesAndGame(websites, req.params.game, function(articles){
+				res.send(articles);
+			});
 		},
 
 		findByWebsiteAndGame: function(req, res) {
 			db.collection('articles').find({website: req.params.website, category: req.params.game}).sort({ pubDate: -1 }).limit(40).toArray(function(err, articles){
 				if(!err) res.send(articles);
 			});
-		},
+		}
 
 
 		// removeAll: function(req, res) {
