0e8a479f67479f8467343730b9e4def9e68eec4b
Antoine Riviere <aereivir@gmail.com>
2013-12-08 15:51:34 +0100

    vakarm coverage

--- feeds_cron.js
+++ feeds_cron.js
@@ -20,6 +20,7 @@ define(['cron', 'Feed', 'Website'], function(cron, Feed, Website){
 			new Feed("Team aAa", "teamaaa", "http://www.team-aaa.com/xml/news-rss-all.xml", "fr", null);
 			new Feed("TeamLiquid", "teamliquid", "http://www.teamliquid.net/rss/news.xml", "en", null);
 			new Feed("VaKarM", "vakarm", "http://feeds2.feedburner.com/vakarm", "fr", "csgo");
+			new Feed("VaKarM", "vakarm", "http://feeds2.feedburner.com/vakarm_coverage", "fr", "csgo");
 			new Feed("HLTV", "hltv", "http://www.hltv.org/news.rss.php", "en", "csgo");
 			new Feed("Esport Actu", "esportactu", "http://www.esportactu.fr/rss.xml", "fr", null);
 			// new Feed("Cadred", "cadred", "http://www.cadred.org/Rss/?type=news", "en", null);
